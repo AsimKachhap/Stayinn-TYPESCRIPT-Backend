5e3fc7938184cf860031207347f40d0df4ace2b6
unknown <asim26k@gmail.com>
2023-05-06 06:41:48 +0530

    MONGODB Connection Establlished

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import mongoose from "mongoose";
 import morgan from "morgan";
 import dotenv from "dotenv";
 
@@ -6,11 +7,25 @@ const app = express();
 app.use(morgan("dev"));
 dotenv.config();
 
+(async (): Promise<void> => {
+  try {
+    await mongoose.connect(process.env.MONGODB_CONNECTION, {
+      useNewUrlParser: true,
+    } as mongoose.ConnectOptions);
+    console.log("CONNECTED TO MONGODB SERVER");
+  } catch (error) {
+    console.log("MONGODB CONNECTION ERROR");
+    console.log(error);
+  }
+})();
+
 app.get("/", (req: express.Request, res: express.Response) => {
   res.send("Welcome to HomePage.");
 });
 
-app.listen(8080, () => {
-  console.log("SERVER IS UP AND RNNING ON PORT 8080");
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+  console.log(`SERVER IS UP AND RUNNING ON PORT ${PORT}`);
   console.log(process.env.HELLO);
 });
